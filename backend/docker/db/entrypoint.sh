f00ad428fe3c359e3d4091fcbf3a1dfa78c5627d
artitzhu <artitzhu95@gmail.com>
2026-07-29 13:47:47 +0200

    fix: use official postgres:16-alpine image, remove custom db Dockerfile

--- backend/docker/db/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-if [ -s "$PGDATA/PG_VERSION" ]; then
-    echo "host all all 0.0.0.0/0 trust" >> "$PGDATA/pg_hba.conf"
-    echo "host all all ::/0 trust" >> "$PGDATA/pg_hba.conf"
-fi
-
-exec /usr/local/bin/docker-entrypoint.sh "$@"
